c4870af3eee6fd9521a5219a9576d9c578b7e50f
Loadszz <loasdzz94@gmail.com>
2023-02-14 10:45:52 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import './App.scss';
+import kef from './kef.jpg';
+
+
+function App() {
+	const [open, setOpen] = useState(false)
+
+	return (
+		<div className="page">
+			<div className='container'>
+
+			</div>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Result.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Result = () => {
+	return (
+		<div className='result'>
+			<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKV2p2GLtPdMOO-cd2e9izWTox6r_8ZlNirA&usqp=CAU" alt="" />
+			<h2>Вы отгадали 3 ответа из 10</h2>
+			<button>Попробовать снова</button>
+			<button>dadas</button>
+		</div>
+	);
+}
+
+export default Result
