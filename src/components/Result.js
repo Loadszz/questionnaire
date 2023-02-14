736355cea9386cdf81240f2aa8d4a3245c56be7f
Loadszz <loasdzz94@gmail.com>
2023-02-14 13:23:01 +0200

    add log

--- src/components/Result.js
+++ src/components/Result.js
@@ -6,6 +6,7 @@ const Result = () => {
 			<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKV2p2GLtPdMOO-cd2e9izWTox6r_8ZlNirA&usqp=CAU" alt="" />
 			<h2>Вы отгадали 3 ответа из 10</h2>
 			<button>Попробовать снова</button>
+			{console.log(lol)}
 			dadas
 		</div>
 	);
